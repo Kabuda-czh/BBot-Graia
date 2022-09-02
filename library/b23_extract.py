13016a86b7b8ffdd732c9806ce3da869ca9efa8c
djkcyl <cyl@cyllive.cn>
2022-09-02 19:57:34 +0800

    修复某些情况下 b23 解析不到的问题

--- library/b23_extract.py
+++ library/b23_extract.py
@@ -5,7 +5,7 @@ import httpx
 async def b23_extract(text: str):
     if "b23.tv" not in text:
         return None
-    if not (b23 := re.compile(r"b23.tv[\/]+(\w+)").search(text)):
+    if not (b23 := re.compile(r"b23.tv[\\/]+(\w+)").search(text)):
         return None
     try:
         url = f"https://b23.tv/{b23[1]}"
