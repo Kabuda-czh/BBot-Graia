28e7e791fbfe965a2ce3e32aba542e9410b94f10
A60 <59153990+djkcyl@users.noreply.github.com>
2022-11-27 18:10:38 +0800

    Update install-bbot.ps1

--- install-bbot.ps1
+++ install-bbot.ps1
@@ -1,11 +1,32 @@
-$pythonVersion = "3.9.13"
+$pythonVersion = "3.9"
+$installPathName = "bbot"
+$gitRepo = "https://github.com/djkcyl/BBot-Graia.git"
+$gitBranch = "web"
 
+
+$pythonPath = "python/Scripts/python.exe"
+$needClear = $args[0] -eq "--clear"
+$wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
 $gitInstalled = Get-Command git -ErrorAction SilentlyContinue
+$pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
+
+if ($needClear) {
+    Write-Host "Clearing ./$installPathName"
+    Remove-Item -Path $installPathName -Recurse -Force
+}
+
+
+function InstallScoop {
+    Write-Host "Installing Scoop"
+    Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
+
+
 if ($gitInstalled) {
     Write-Host "Git is already installed"
 }
 else {
-    $wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
     if ($wingetInstalled) {
         Write-Host "Installing Git with winget"
         & winget install --id Git.Git
@@ -13,87 +34,76 @@ else {
     else {
         Write-Host "Installing Git with Scoop"
         if (!(Test-Path "$env:USERPROFILE\scoop")) {
-            Write-Host "Installing Scoop"
-            Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+            InstallScoop
         }
         & scoop install git
     }
 }
 
-Write-Host "Cloning BBot"
-& git clone https://github.com/djkcyl/BBot-Graia.git
+Write-Host "Cloning $gitRepo"
+& git clone $gitRepo -b $gitBranch $installPathName
 
-# 判断是否已安装 Python 3.9
-$pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
+function CreatePythonVenv {
+    & python -m venv $installPathName/python
+}
+
+
 $pythonInstalledVersion = $pythonInstalled.Version
 if ($pythonInstalled -and $pythonInstalled.Version.Major -eq 3 -and ($pythonInstalled.Version.Minor -eq 9 -or $pythonInstalled.Version.Minor -eq 10)) {
     Write-Host "Python $pythonInstalledVersion is already installed"
-    & python -m venv python
-    $pythonPath = "python/Scripts/python.exe"
+    CreatePythonVenv
 }
 else {
-    $pythonZip = ".\python-$pythonVersion-embed-amd64.zip"
-    $pythonUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion-embed-amd64.zip"
-    Write-Host "Downloading Python $pythonVersion"
-    Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonZip
-    Write-Host "Extracting Python $pythonVersion"
-    Expand-Archive -Path $pythonZip -DestinationPath ".\python" -Force
-    $pythonPath = ".\python\python.exe"
-    Write-Host "Installing pip"
-    Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
-    & $pythonPath ./get-pip.py
-    (Get-Content -Path ./python/python39._pth -Raw) -replace "#import site", "import site" | Set-Content -Path ./python/python39._pth
-    & "$pythonEmbed -m pip install --upgrade pip"
-    Remove-Item -Path $pythonZip
-    Remove-Item -Path ./get-pip.py
+    if ($wingetInstalled) {
+        Write-Host "Installing Python $pythonVersion with winget"
+        & winget install --id Python.Python.$pythonVersion
+        CreatePythonVenv
+    }
+    else {
+        Write-Host "Installing Python $pythonVersion with Scoop"
+        & scoop install python@$pythonVersion
+        CreatePythonVenv
+    }
 }
-
 
 Write-Host "Installing dependencies"
-& $pythonPath -m pip install -r ./BBot-Graia/requirements.txt
+& $installPathName/$pythonPath -m pip install -r ./$installPathName/requirements.txt
 
 
-# 写入启动脚本
-$script = "cd ./BBot-Graia
-../$pythonPath ./main.py"
-Set-Content -Path ./start-bbot.ps1 -Value $script
+$script = "Set-Location ./$installPathName
+$pythonPath ./main.py
+Set-Location .."
+Set-Content -Path "./start-$installPathName.ps1" -Value $script
 
-# 写入更新脚本
 $script = "`$process = Get-Process -Name 'python' -ErrorAction SilentlyContinue
 if (`$process) {
     `$process | Format-Table -AutoSize
-    `$choice = Read-Host -Prompt 'There are currently Python processes running, choose whether to force close all Python processes and update? (y/n)'
-    if (`$choice -eq 'y') {
-        `$process | Stop-Process -Force
-    }
-    else {
+    `$choice = Read-Host -Prompt 'There are currently Python processes running, choose whether to force an update? (y/n)'
+    if !(`$choice -eq 'y') {
         exit
     }
 }
-& cd ./BBot-Graia
+& Set-Location ./$installPathName
 & git pull
-& ../$pythonPath -m pip install -r ./requirements.txt --upgrade"
-Set-Content -Path ./update-bbot.ps1 -Value $script
+& $pythonPath -m pip install -r ./requirements.txt --upgrade
+Set-Location .."
+Set-Content -Path "./update-$installPathName.ps1" -Value $script
 
-# 写入卸载脚本
 $script = "`$process = Get-Process -Name 'python' -ErrorAction SilentlyContinue
 if (`$process) {
     `$process | Format-Table -AutoSize
-    `$choice = Read-Host -Prompt 'There are currently Python processes running, choose whether to force close all Python processes and update? (y/n)'
-    if (`$choice -eq 'y') {
-        `$process | Stop-Process -Force
-    }
-    else {
+    `$choice = Read-Host -Prompt 'There are currently Python processes running, choose whether to force an update? (y/n)'
+    if !(`$choice -eq 'y') {
         exit
     }
 }
-Remove-Item -Path ./BBot-Graia -Recurse -Force
-Remove-Item -Path ./start-bbot.ps1 -Force
-Remove-Item -Path ./update-bbot.ps1 -Force
-Remove-Item -Path ./uninstall-bbot.ps1 -Force"
-Set-Content -Path ./uninstall-bbot.ps1 -Value $script
+Remove-Item -Path ./$installPathName -Recurse -Force
+Remove-Item -Path ./start-$installPathName.ps1 -Force
+Remove-Item -Path ./update-$installPathName.ps1 -Force
+Remove-Item -Path ./uninstall-$installPathName.ps1 -Force"
+Set-Content -Path "./uninstall-$installPathName.ps1" -Value $script
 
 Write-Host "Installation completed"
-Write-Host "Run start-bbot.ps1 to start the bot"
-Write-Host "Run update-bbot.ps1 to update the bot"
-Write-Host "Run uninstall-bbot.ps1 to uninstall the bot"
+Write-Host "Run start-$installPathName.ps1 to start the $installPathName"
+Write-Host "Run update-$installPathName.ps1 to update the $installPathName"
+Write-Host "Run uninstall-$installPathName.ps1 to uninstall the $installPathName"
